2012c35e5b6c397fffd134b5ff672d78fd1e41ba
Piotr <piotrek.g.aleksandrowicz@gmail.com>
2021-08-30 12:25:37 +0200

    Refactoring RegisterForm3.js

--- cypress/integration/myTests/RegisterForm3.js
+++ cypress/integration/myTests/RegisterForm3.js
@@ -5,18 +5,25 @@ describe('Register test, fill inputs usnig array of elements', () => {
     })
 
     it('Use array of inputs to fill input fields', () => {
-        cy.get('.input-field').eq(0).clicl().type('Piotr')
-        cy.get('.input-field').eq(1).clicl().type('Aleksandrowicz')
-        cy.get('.input-field').eq(2).clicl().type('piotraleksandrowicz@pushpushgo.com')
-        cy.get('.input-field').eq(3).clicl().type('piotraleksandrowicz@pushpushgo.com')
-        cy.get('.input-field').eq(4).clicl().type('DCRvx1000eMK!')
-        cy.get('.input-field').eq(5).clicl().type('DCRvx1000eMK!')
+        cy.get('.input-field').eq(0).click().type('Piotr')
+        cy.get('.input-field').eq(1).click().type('Aleksandrowicz')
+        cy.get('.input-field').eq(2).click().type('piotraleksandrowicz@pushpushgo.com')
+        cy.get('.input-field').eq(3).click().type('piotraleksandrowicz@pushpushgo.com')
+        cy.get('.input-field').eq(4).click().type('DCRvx1000eMK!')
+        cy.get('.input-field').eq(5).click().type('DCRvx1000eMK!')
+    })
+
+    it('Checking lengh of array eye button', () => {
+        cy.get("[class='eye-button hide-eye']").its('length').should('eq', 2)
+    })
+
+    it('Pause dabug', () => {
+        cy.pause()
     })
 
     it('Click an eye button to show passwords', () => {
-        cy.get('.eye-button hide-eye').eq(0).click()
-        cy.get('.eye-button hide-eye').eq(1).click()
-
+        cy.get("[role='button']").eq(0).click()
+        cy.get("[role='button']").eq(1).click()
     })
     
 })
