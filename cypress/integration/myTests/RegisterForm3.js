b025c44800e2316023060bb81e0facc2496c3f89
Piotr <piotrek.g.aleksandrowicz@gmail.com>
2021-08-30 12:00:38 +0200

    add RegisterForm3.js

---
+++ cypress/integration/myTests/RegisterForm3.js
@@ -0,0 +1,22 @@
+describe('Register test, fill inputs usnig array of elements', () => {
+
+    it('Get into registration form', () => {
+        cy.visit('https://app.pushpushgo.com/register')
+    })
+
+    it('Use array of inputs to fill input fields', () => {
+        cy.get('.input-field').eq(0).clicl().type('Piotr')
+        cy.get('.input-field').eq(1).clicl().type('Aleksandrowicz')
+        cy.get('.input-field').eq(2).clicl().type('piotraleksandrowicz@pushpushgo.com')
+        cy.get('.input-field').eq(3).clicl().type('piotraleksandrowicz@pushpushgo.com')
+        cy.get('.input-field').eq(4).clicl().type('DCRvx1000eMK!')
+        cy.get('.input-field').eq(5).clicl().type('DCRvx1000eMK!')
+    })
+
+    it('Click an eye button to show passwords', () => {
+        cy.get('.eye-button hide-eye').eq(0).click()
+        cy.get('.eye-button hide-eye').eq(01).click()
+
+    })
+    
+})
--- cypress/integration/myTests/RegistrationForm2.js
+++ cypress/integration/myTests/RegistrationForm2.js
@@ -3,8 +3,28 @@ describe('Checking all inputs are displayed', () => {
     it('get into registration form', () =>{
         cy.visit('https://app.pushpushgo.com/register')
     })
-    
+
     it('count number of input fields', () => {
         cy.get('.input-field').its('length').should('eq', 6)
     })
+
+    it ('checking names of inputs', () => {
+        cy.xpath('//*[@id="ppg-app"]/div/div/div/div/div/div[2]/div/form/div[1]/label').contains('Name')
+        cy.xpath('//*[@id="ppg-app"]/div/div/div/div/div/div[2]/div/form/div[2]/label').contains('Last name')
+        cy.xpath('//*[@id="ppg-app"]/div/div/div/div/div/div[2]/div/form/div[3]/label').contains('E-mail')
+        cy.xpath('//*[@id="ppg-app"]/div/div/div/div/div/div[2]/div/form/div[4]/label').contains('Company or Organization name')
+        cy.xpath('//*[@id="ppg-app"]/div/div/div/div/div/div[2]/div/form/div[5]/div/label').contains('Password')
+        cy.xpath('//*[@id="ppg-app"]/div/div/div/div/div/div[2]/div/form/div[6]/div/label').contains('Repeat password')
+    })
+
+    it('checking names of inputs using array', () => {
+        cy.get('label').eq(0).contains('Name')
+        cy.get('label').eq(1).contains('Last name')
+        cy.get('label').eq(2).contains('E-mail')
+        cy.get('label').eq(3).contains('Company or Organization name')
+        cy.get('label').eq(4).contains('Password')
+        cy.get('label').eq(5).contains('Repeat password')
+        
+    })
 })
+
