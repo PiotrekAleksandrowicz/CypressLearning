28f1843675afd07568eebfb61047f5a8eb7f2186
PiotrekAleksandrowicz <piotrek.g.aleksandrowicz@gmail.com>
2021-09-06 13:33:59 +0200

    add variables aliases(for xpaths and css selectors)

--- cypress/integration/myTests/RegisterForm4.js
+++ cypress/integration/myTests/RegisterForm4.js
@@ -3,16 +3,21 @@ describe('Fullfill register form and click on the chackbox', () => {
         cy.visit('https://app.master1.qappg.co/login')
     })
     it('Fill mail and password', () => {
-        cy.get("[id='t-login-username']").click().type('piotraleksandrowicz2@pushpushgo.com')
-        cy.get("[id='t-login-password']").click().type('DCRvx1000eMK!')
+        cy.get("[id='t-login-username']").as('emailInput')
+        cy.get('emailInput').click().type('piotraleksandrowicz2@pushpushgo.com')
+        y.get("[id='t-login-password']").as('passwordInput')
+        cy.get('passwordInput').click().type('DCRvx1000eMK!')
     })
     it('Click checkbox', () => {
-        cy.get("[type='checkbox']").click()
+        cy.get("[type='checkbox']").as('checkbox')
+        cy.get('checkbox').click()
     })
     it('Click login button', () => {
-        cy.get("[type='submit']").click()
+        cy.get("[type='submit']").as('loginButton')
+        cy.get('loginButton').click()
     })
     it('Chek you are logged', () => {
-        cy.get("[class='primary size24 text-normal']").contains('PROJEKT API NORBERT')
+        cy.get("[class='primary size24 text-normal']").as('nameOfProject')
+        cy.get('nameOfProject').contains('PROJEKT API NORBERT')
     })
 })
