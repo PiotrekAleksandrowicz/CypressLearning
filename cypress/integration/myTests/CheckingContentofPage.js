94894ba72a208f97b680535fb8793bde52f3a939
PiotrekAleksandrowicz <piotrek.g.aleksandrowicz@gmail.com>
2021-09-23 13:17:47 +0200

    new test with checking content of website

--- cypress/integration/myTests/CheckingContentofPage.js
+++ cypress/integration/myTests/CheckingContentofPage.js
@@ -1,12 +1,11 @@
-const cypress = require("cypress")
-const { it } = require("mocha")
-
-describe('Test of checking content of webSite', () =>{
-
-    it('check into webSite https://testqapa1.stppg.co/', () => {
-
+describe('Test of content of website', () => {
+  
+    it('visit webSite', ()=>{
         cy.visit('https://testqapa1.stppg.co/')
         cy.wait(2000)
+    })
+
+    it('Check content of website', () =>{
         cy.document()
             .should('have.property','src')
             .and('eq', 'https://s-eu-1.pushpushgo.com/js/605d936beb6b8169f2bf6188.js')
