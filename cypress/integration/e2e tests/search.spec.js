1ff185a2262340000227dabd800d394a5d144897
PiotrekAleksandrowicz <piotrek.g.aleksandrowicz@gmail.com>
2021-09-27 11:16:20 +0200

    refactoring code

--- cypress/integration/e2e tests/search.spec.js
+++ cypress/integration/e2e tests/search.spec.js
@@ -1,14 +1,12 @@
-const cypress = require("cypress")
-const { it } = require("mocha")
+describe('Search Box test', () => {
 
-describe('Search Box test',() => {
-
-    before(function(){
-        cy.visit('http://http://zero.webappsecurity.com/')
+    before(function() {
+        cy.visit('http://zero.webappsecurity.com/')
+        cy.wait(5000)
     })
 
     it('Shoud type some text into search box', () => {
-        cypress.get('[id="searchTerm"]').type('some text {enter}')
+        cy.get("input[id='searchTerm']").type('some text {enter}')
     })
 
     it('Should show sear results page ', () => {
