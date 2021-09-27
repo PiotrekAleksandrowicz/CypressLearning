680463588b4f7170995c20b21d0f7f2b282fb357
PiotrekAleksandrowicz <piotrek.g.aleksandrowicz@gmail.com>
2021-09-27 11:43:58 +0200

    forgotten password test

---
+++ cypress/integration/e2e tests/forgottenPassword.spec.js
@@ -0,0 +1,26 @@
+describe('Forgotten pasword test', () => {
+
+    before(function(){
+        cy.visit('http://zero.webappsecurity.com/index.html')
+    })
+
+    it('Click signUp button', () => {
+        cy.get('button[id="signin_button"]').click()
+    })
+
+    it('Click forgotten pasword', () => {
+        cy.get('.offset3 > a').click()
+    })
+
+    it('Print email to input', () => {
+        cy.get('input[id="user_email"]').type('piotr271993@wp.pl')
+    })
+
+    it('Click Sign password button', () => {
+        cy.get('button[type="submit"]').click
+    })
+
+    it('Result page should have typed email', () => {
+        cy.get('.offset3 span6 > div > div').contains('Your password will be sent to the following email: piotr271993@wp.pl')
+    })
+})
